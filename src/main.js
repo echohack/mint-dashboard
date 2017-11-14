8d27099a6f3c237d05e43a1e21f8c601e28f7f5d
t889096 <michael.kirkpatrick@syngenta.com>
2017-11-14 17:07:25 -0500

    Made the branch names dynamic in App.vue.
    Cleanup in main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-new */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
@@ -7,10 +8,10 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue);
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
-
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
